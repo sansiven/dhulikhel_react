abba3418c429eb5c11607ea68365c3fee2d0017d
sansiven <sansiven@gmail.com>
2022-05-10 13:10:18 +0545

    added mailing list to the dashboard

--- src/Routes.js
+++ src/Routes.js
@@ -1,67 +1,182 @@
-import React from 'react';
-import './resources/css/app.css';
-import './resources/css/bootstrap.min.css';
+import React from "react";
+import "./resources/css/app.css";
+import "./resources/css/bootstrap.min.css";
 import { Switch } from "react-router-dom";
-import Layout from './HOC/Layout'
-import PrivateRoutes from './components/authRoutes/PrivateRoutes';
-import PublicRoutes from './components/authRoutes/PublicRoutes';
+import Layout from "./HOC/Layout";
+import PrivateRoutes from "./components/authRoutes/PrivateRoutes";
+import PublicRoutes from "./components/authRoutes/PublicRoutes";
 
+import Home from "./components/home";
+import Gallery from "./components/gallery";
+import Sign_In from "./components/signin";
+import Services from "./components/services";
+import Accomodation from "./components/accomodation";
+import SingleAccomodation from "./components/accomodation/SingleAccomodation";
+import AdminMessages from "./components/admin/messages/AdminMesaages";
+import AdminGallery from "./components/admin/gallery";
+import AdminRooms from "./components/admin/rooms";
+import AddEditRooms from "./components/admin/rooms/addEditRooms";
+import AdminServices from "./components/admin/services";
+import AdminTariff from "./components/admin/tariffs";
+import Tariffs from "./components/admin/tariffs/Tariffs";
+import AdminAbout from "./components/admin/about";
+import NotFound from "./components/ui/not_found";
 
-import Home from './components/home'
-import Gallery from './components/gallery';
-import Sign_In from './components/signin';
-import Services from './components/services';
-import Accomodation from './components/accomodation';
-import SingleAccomodation from './components/accomodation/SingleAccomodation';
-import AdminMessages from './components/admin/messages/AdminMesaages';
-import AdminGallery from './components/admin/gallery';
-import AdminRooms from './components/admin/rooms';
-import AddEditRooms from './components/admin/rooms/addEditRooms';
-import AdminServices from './components/admin/services';
-import AdminTariff from './components/admin/tariffs';
-import Tariffs from './components/admin/tariffs/Tariffs'
-import AdminAbout from './components/admin/about';
-import NotFound from './components/ui/not_found';
-
-import Dashboard from './components/admin/Dashboard';
-import AddEditServices from './components/admin/services/addEditServices';
-import AddEditReviews from './components/admin/reviews/addEditReviews';
-import AdminReviews from './components/admin/reviews'
+import Dashboard from "./components/admin/Dashboard";
+import AddEditServices from "./components/admin/services/addEditServices";
+import AddEditReviews from "./components/admin/reviews/addEditReviews";
+import AdminReviews from "./components/admin/reviews";
+import AdminSubscribed from "./components/admin/subscriptions/AdminSubscribed";
 
 const Routes = (props) => {
-  console.log(props)
+  console.log(props);
 
   return (
     <Layout>
       <Switch>
-        <PrivateRoutes {...props} path="/admin_reviews/add_review/:id" exact component={AddEditReviews} />
-        <PrivateRoutes {...props} path="/admin_reviews/add_review" exact component={AddEditReviews} />
-        <PrivateRoutes {...props} path="/admin_reviews" exact component={AdminReviews} />
-        <PrivateRoutes {...props} path="/admin_services/add_service/:id" exact component={AddEditServices} />
-        <PrivateRoutes {...props} path="/admin_services/add_service" exact component={AddEditServices} />
-        <PrivateRoutes {...props} path="/admin_services" exact component={AdminServices} />
-        <PrivateRoutes {...props} path="/admin_rooms/add_room" exact component={AddEditRooms} />
-        <PrivateRoutes {...props} path="/admin_rooms/add_rooms/:id" exact component={AddEditRooms} />
-        <PrivateRoutes {...props} path="/admin_rooms" exact component={AdminRooms} />
-        <PrivateRoutes {...props} path="/tariffs/add_tariff/:id" exact component={AdminTariff} />
-        <PrivateRoutes {...props} path="/tariffs/add_tariff" exact component={AdminTariff} />
+        <PrivateRoutes
+          {...props}
+          path="/admin_reviews/add_review/:id"
+          exact
+          component={AddEditReviews}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_reviews/add_review"
+          exact
+          component={AddEditReviews}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_reviews"
+          exact
+          component={AdminReviews}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_services/add_service/:id"
+          exact
+          component={AddEditServices}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_services/add_service"
+          exact
+          component={AddEditServices}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_services"
+          exact
+          component={AdminServices}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_rooms/add_room"
+          exact
+          component={AddEditRooms}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_rooms/add_rooms/:id"
+          exact
+          component={AddEditRooms}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_rooms"
+          exact
+          component={AdminRooms}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/tariffs/add_tariff/:id"
+          exact
+          component={AdminTariff}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/tariffs/add_tariff"
+          exact
+          component={AdminTariff}
+        />
         <PrivateRoutes {...props} path="/tariffs" exact component={Tariffs} />
-        <PrivateRoutes {...props} path="/admin_gallery" exact component={AdminGallery} />
-        <PrivateRoutes {...props} path="/admin_messages" exact component={AdminMessages}/>
-        <PrivateRoutes {...props} path="/admin_about" exact component={AdminAbout}/>
-        <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard}/>
-        <PublicRoutes {...props} restricted={true} path="/sign_in" exact component={Sign_In} />
-        <PublicRoutes {...props} restricted={false} path="/gallery" exact component={Gallery} />
-        <PublicRoutes {...props} restricted={false} path="/accomodation/:id" exact component={SingleAccomodation} />
-        <PublicRoutes {...props} restricted={false} path="/accomodation" exact component={Accomodation} />
-        <PublicRoutes {...props} restricted={false} path="/services" exact component={Services} />
-        <PublicRoutes {...props} restricted={false} path="/" exact component={Home} />
+        <PrivateRoutes
+          {...props}
+          path="/admin_gallery"
+          exact
+          component={AdminGallery}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_messages"
+          exact
+          component={AdminMessages}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_about"
+          exact
+          component={AdminAbout}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/admin_subscriptions"
+          exact
+          component={AdminSubscribed}
+        />
+        <PrivateRoutes
+          {...props}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={true}
+          path="/sign_in"
+          exact
+          component={Sign_In}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={false}
+          path="/gallery"
+          exact
+          component={Gallery}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={false}
+          path="/accomodation/:id"
+          exact
+          component={SingleAccomodation}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={false}
+          path="/accomodation"
+          exact
+          component={Accomodation}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={false}
+          path="/services"
+          exact
+          component={Services}
+        />
+        <PublicRoutes
+          {...props}
+          restricted={false}
+          path="/"
+          exact
+          component={Home}
+        />
         <PublicRoutes {...props} restricted={false} component={NotFound} />
-
-        
       </Switch>
     </Layout>
   );
-}
+};
 
 export default Routes;
--- src/components/admin/nav/AdminNav.js
+++ src/components/admin/nav/AdminNav.js
@@ -1,81 +1,87 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import ListItem from '@material-ui/core/ListItem';
-import {firebase} from '../../../firebase'
+import React from "react";
+import { Link } from "react-router-dom";
+import ListItem from "@material-ui/core/ListItem";
+import { firebase } from "../../../firebase";
 
 const AdminNav = () => {
+  const links = [
+    {
+      title: "Messages",
+      linkTo: "/admin_messages",
+    },
+    {
+      title: "Newsletter Subscriptions",
+      linkTo: "/admin_subscriptions",
+    },
+    {
+      title: "Add Photos",
+      linkTo: "/admin_gallery",
+    },
+    {
+      title: "Tariffs",
+      linkTo: "/tariffs",
+    },
+    {
+      title: "Add Tariffs",
+      linkTo: "/tariffs/add_tariff",
+    },
+    {
+      title: "Rooms",
+      linkTo: "/admin_rooms",
+    },
+    {
+      title: "Add Rooms",
+      linkTo: "/admin_rooms/add_room",
+    },
+    {
+      title: "Services",
+      linkTo: "/admin_services",
+    },
+    {
+      title: "About Content",
+      linkTo: "/admin_about",
+    },
+    {
+      title: "Reviews",
+      linkTo: "/admin_reviews",
+    },
+  ];
 
-    const links = [
-        {
-            title: 'Messages',
-            linkTo: '/admin_messages'
-        },
-        {
-            title: 'Add Photos',
-            linkTo: '/admin_gallery'
-        },
-        {
-            title: 'Tariffs',
-            linkTo: '/tariffs'
-        },
-        {
-            title: 'Add Tariffs',
-            linkTo: '/tariffs/add_tariff'
-        },
-        {
-            title: 'Rooms',
-            linkTo: '/admin_rooms'
-        },
-        {
-            title: 'Add Rooms',
-            linkTo: '/admin_rooms/add_room'
-        },
-        {
-            title: 'Services',
-            linkTo: '/admin_services'
-        },
-        {
-            title: 'About Content',
-            linkTo: '/admin_about'
-        },
-        {
-            title: 'Reviews',
-            linkTo: '/admin_reviews'
-        }
-    ]
+  const style = {
+    color: "#ffffff",
+    fontWeight: "300",
+    borderBottom: "1px solid #353535",
+  };
 
-    const style = {
-        color: '#ffffff',
-        fontWeight: '300',
-        borderBottom: '1px solid #353535'
-    }
+  const renderItems = () =>
+    links.map((link) => (
+      <Link to={link.linkTo} key={link.title}>
+        <ListItem button style={style}>
+          {link.title}
+        </ListItem>
+      </Link>
+    ));
 
-    const renderItems = () => (
-        links.map((link) => (
-            <Link to={link.linkTo} key={link.title}>
-                <ListItem button style={style}>
-                    {link.title}
-                </ListItem>
-            </Link>
-        ))
-    )
+  const logOutHandler = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        console.log("logged out suucesfully");
+      })
+      .catch((e) => {
+        console.log("somethings wrong", e);
+      });
+  };
 
-    const logOutHandler = () => {
-        firebase.auth().signOut().then(()=>{
-            console.log('logged out suucesfully')
-        }).catch((e)=>{
-            console.log('somethings wrong', e)
-        })
-    }
-
-    return (
-        <div>
-            {renderItems()}
-            <ListItem button style={style} onClick={()=> logOutHandler()}>
-                Log Out
-            </ListItem>
-        </div>
-    );
+  return (
+    <div>
+      {renderItems()}
+      <ListItem button style={style} onClick={() => logOutHandler()}>
+        Log Out
+      </ListItem>
+    </div>
+  );
 };
 
 export default AdminNav;
