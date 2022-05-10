efe65e59a775a248bea347b0c80c7eead5f8d7fe
sansiven <sansiven@gmail.com>
2022-05-10 12:54:49 +0545

    added timestamp to show date and time when mesaged

--- src/components/header_footer/navigation/index.js
+++ src/components/header_footer/navigation/index.js
@@ -1,94 +1,96 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import AppBarCollapse from "./AppBarCollapse";
-import {HotelLogo} from '../../ui/Logo';
-import {WhiteHotelLogo} from '../../ui/whiteLogo';
-
+import { HotelLogo } from "../../ui/Logo";
+import { WhiteHotelLogo } from "../../ui/whiteLogo";
 
 class Navigation extends Component {
+  state = {
+    headerTransparent: true,
+  };
 
-    state = {
-        headerTransparent : true
-    }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
-    componentDidMount(){
-        window.addEventListener('scroll', this.handleScroll)
-    }
-
-    //if not needed to make header transparent
-    /* componentWillUnmount(){
+  //if not needed to make header transparent
+  /* componentWillUnmount(){
         window.removeEventListener('scroll');
     } */
 
-    handleScroll = () => {
-        if(window.scrollY > 0){
-            this.setState({
-                headerTransparent: false
-            })
-        }else{
-            this.setState({
-                headerTransparent: true
-            })
-        }
+  handleScroll = () => {
+    if (window.scrollY > 0) {
+      this.setState({
+        headerTransparent: false,
+      });
+    } else {
+      this.setState({
+        headerTransparent: true,
+      });
     }
+  };
 
-    render() {
-        return (
-            <AppBar 
-                position="fixed"
-                style={{
-                    backgroundColor: this.state.headerTransparent ? "transparent" : "#09779A",
-                    boxShadow:"none",
-                    padding: '0',
-                    borderColor: '2px solid #70798c'/*dont know why used*/
-                }}
-            >
-                <Toolbar style={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center'
-                    }}>
-                    <Typography
-                        /* variant="title" */
-                        color="inherit"
-                        className=""
-                    >
-                        <div style={{flexGrow:1}}>
-                            <div className="logo">
-                                {console.log(this.props.forLogo)}
-                                {this.state.headerTransparent ?
-                                    <HotelLogo
-                                        link={true}
-                                        linkTo="/"
-                                        width ="80px"
-                                        height="80px" 
-                                        bckSize= "80px"
-                                    /> 
-                                    : <WhiteHotelLogo
-                                        link={true}
-                                        linkTo="/"
-                                        width ="80px"
-                                        height="80px" 
-                                        bckSize= "80px"
-                                    />
-                                }
-                                {/* <WhiteHotelLogo
+  render() {
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: this.state.headerTransparent
+            ? "transparent"
+            : "#09779A",
+          boxShadow: "none",
+          padding: "0",
+          borderColor: "2px solid #70798c" /*dont know why used*/,
+        }}
+      >
+        <Toolbar
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            /* variant="title" */
+            color="inherit"
+            className=""
+          >
+            <div style={{ flexGrow: 1 }}>
+              <div className="logo">
+                {this.state.headerTransparent ? (
+                  <HotelLogo
+                    link={true}
+                    linkTo="/"
+                    width="80px"
+                    height="80px"
+                    bckSize="80px"
+                  />
+                ) : (
+                  <WhiteHotelLogo
+                    link={true}
+                    linkTo="/"
+                    width="80px"
+                    height="80px"
+                    bckSize="80px"
+                  />
+                )}
+                {/* <WhiteHotelLogo
                                     link={true}
                                     linkTo="/"
                                     width ="80px"
                                     height="80px" 
                                     bckSize= "80px"
                                 /> */}
-                            </div>
-                        </div>
-                    </Typography>
-                    <AppBarCollapse />
-                </Toolbar>
-            </AppBar>
-        );
-    }
+              </div>
+            </div>
+          </Typography>
+          <AppBarCollapse />
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
 export default Navigation;
--- src/components/home/contact/MessageUs/index.js
+++ src/components/home/contact/MessageUs/index.js
@@ -1,172 +1,177 @@
-import React, { Component } from 'react';
-import Roll from 'react-reveal/Roll';
-import FormField from '../../../ui/formFields';
-import {validate} from '../../../ui/misc';
-import Button from '@material-ui/core/Button';
-import {firebaseMessages} from '../../../../firebase'; 
+import React, { Component } from "react";
+import Roll from "react-reveal/Roll";
+import FormField from "../../../ui/formFields";
+import { validate } from "../../../ui/misc";
+import Button from "@material-ui/core/Button";
+import { firebaseMessages } from "../../../../firebase";
+import firebase from "firebase";
 
 class MessageUs extends Component {
+  state = {
+    formError: false,
+    formSuccess: "",
+    formdata: {
+      name: {
+        element: "input",
+        value: "",
+        config: {
+          name: "name_input",
+          type: "text",
+          placeholder: "Your name",
+          class: "form-control",
+        },
+        validation: {
+          required: true,
+        },
+        valid: false,
+        validationMessage: "",
+      },
+      email: {
+        element: "input",
+        value: "",
+        config: {
+          name: "email_input",
+          type: "email",
+          placeholder: "email as example@email.com",
+          class: "form-control",
+        },
+        validation: {
+          required: true,
+          email: true,
+        },
+        valid: false,
+        validationMessage: "",
+      },
+      message: {
+        element: "textarea",
+        value: "",
+        config: {
+          name: "message_input",
+          type: "text",
+          placeholder: this.props.placeholder
+            ? this.props.placeholder
+            : "Your Message",
+          class: "form-control text-area",
+        },
+        validation: {
+          required: true,
+        },
+        valid: false,
+        validationMessage: "",
+      },
+    },
+  };
 
-    state = {
-        formError: false,
-        formSuccess: '',
-        formdata: {
-            name:{
-                element: 'input',
-                value: '',
-                config: {
-                    name: 'name_input',
-                    type: 'text',
-                    placeholder: 'Your name',
-                    class: 'form-control'
-                },
-                validation:{
-                    required: true,
-                },
-                valid: false,
-                validationMessage: ''
-            },
-            email:{
-                element: 'input',
-                value: '',
-                config: {
-                    name: 'email_input',
-                    type: 'email',
-                    placeholder: 'email as example@email.com',
-                    class: 'form-control'
-                },
-                validation:{
-                    required: true,
-                    email: true
-                },
-                valid: false,
-                validationMessage: ''
-            },
-            message:{
-                element: 'textarea',
-                value: '',
-                config: {
-                    name: 'message_input',
-                    type: 'text',
-                    placeholder: this.props.placeholder ? this.props.placeholder : "Your Message",
-                    class: 'form-control text-area'
-                },
-                validation:{
-                    required: true,
-                },
-                valid: false,
-                validationMessage: ''
-            }
-        }
+  updateForm(element) {
+    //not good to mutate the state so copy the state and work on this
+    const newFormData = { ...this.state.formdata };
+    //get the email state
+    const newElement = { ...newFormData[element.id] };
+
+    newElement.value = element.event.target.value;
+
+    let validData = validate(newElement);
+
+    newElement.valid = validData[0];
+    newElement.validationMessage = validData[1];
+
+    newFormData[element.id] = newElement;
+
+    this.setState({
+      formError: false,
+      formdata: newFormData,
+    });
+    //console.log(this.state.formdata.email.value)
+  }
+
+  resetFormSuccess() {
+    //not good to mutate the state so copy the state and work on this
+    const newFormData = { ...this.state.formdata };
+
+    for (let key in newFormData) {
+      newFormData[key].value = "";
+      newFormData[key].valid = false;
+      newFormData[key].validationMessage = "";
     }
 
-    updateForm(element){
-        //not good to mutate the state so copy the state and work on this
-        const newFormData = { ...this.state.formdata};
-        //get the email state
-        const newElement = {...newFormData[element.id]}
-    
-        newElement.value = element.event.target.value;
+    this.setState({
+      formError: false,
+      formdata: newFormData,
+      formSuccess: "Congratulations",
+    });
 
-        let validData = validate(newElement)
-        
-        newElement.valid = validData[0];
-        newElement.validationMessage = validData[1]
+    this.clearSuccessMessage();
+  }
 
-        newFormData[element.id] = newElement;
-        
-        
-        this.setState({
-            formError: false,
-            formdata: newFormData
-        })
-        //console.log(this.state.formdata.email.value)
+  clearSuccessMessage() {
+    setTimeout(() => {
+      this.setState({ formSuccess: "" });
+    }, 2000);
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+    let dataToSubmit = {};
+    let formIsValid = true;
+
+    for (let key in this.state.formdata) {
+      dataToSubmit[key] = this.state.formdata[key].value;
+      formIsValid = this.state.formdata[key].valid && formIsValid;
     }
 
-    resetFormSuccess(){
-        //not good to mutate the state so copy the state and work on this
-        const newFormData = { ...this.state.formdata};
+    dataToSubmit.createdAt = firebase.firestore.Timestamp.now().seconds;
 
-        for(let key in newFormData){
-            newFormData[key].value = '';
-            newFormData[key].valid = false;
-            newFormData[key].validationMessage = '';
-        }
+    if (formIsValid) {
+      console.log(dataToSubmit);
+      firebaseMessages.push(dataToSubmit);
 
-        this.setState({
-            formError: false,
-            formdata: newFormData,
-            formSuccess: 'Congratulations'
-        })
-
-        this.clearSuccessMessage();
+      this.resetFormSuccess();
+    } else {
+      this.setState({
+        formError: true,
+      });
     }
+  }
 
-    clearSuccessMessage(){
-        setTimeout(()=>{
-            this.setState({formSuccess: ''});
-        },2000)
-    }
-
-    submitForm(event){
-        event.preventDefault();
-        let dataToSubmit = {};
-        let formIsValid = true;
-
-        for(let key in this.state.formdata){
-            dataToSubmit[key] = this.state.formdata[key].value;
-            formIsValid = this.state.formdata[key].valid && formIsValid;
-        }
-
-        if(formIsValid){
-            console.log(dataToSubmit)
-            firebaseMessages.push(dataToSubmit)
-            
-            this.resetFormSuccess()
-        }else{
-            this.setState({
-                formError: true
-            })
-        }
-
-        
-    }
-
-    render() {
-        return (
-            <Roll right>
-                <div className="message-us" style={{textAlign: 'center'}}>
-                    <form onSubmit={(event) => this.submitForm(event)} className="message-us-form">
-                            <FormField 
-                            id={'name'}
-                            formData = {this.state.formdata.name}
-                            change={(element)=>this.updateForm(element)}
-                        />                            
-                        <FormField 
-                            id={'email'}
-                            formData = {this.state.formdata.email}
-                            change={(element)=>this.updateForm(element)}
-                        />
-                        <FormField 
-                            id={'message'}
-                            formData = {this.state.formdata.message}
-                            change={(element)=>this.updateForm(element)}
-                        />
-                        {this.state.formError ?
-                                <div className="error_label">Something's wrong. Try Again!</div>
-                            :null
-                        }
-                        <div className="success_label">{this.state.formSuccess}</div>
-                        <Button 
-                            variant="contained"  
-                            size="large"
-                            onClick={(event) => this.submitForm(event)}
-                        >{this.props.btnText ? this.props.btnText : "Send Message"}</Button>
-                    </form>
-                </div>
-            </Roll>
-        );
-    }
+  render() {
+    return (
+      <Roll right>
+        <div className="message-us" style={{ textAlign: "center" }}>
+          <form
+            onSubmit={(event) => this.submitForm(event)}
+            className="message-us-form"
+          >
+            <FormField
+              id={"name"}
+              formData={this.state.formdata.name}
+              change={(element) => this.updateForm(element)}
+            />
+            <FormField
+              id={"email"}
+              formData={this.state.formdata.email}
+              change={(element) => this.updateForm(element)}
+            />
+            <FormField
+              id={"message"}
+              formData={this.state.formdata.message}
+              change={(element) => this.updateForm(element)}
+            />
+            {this.state.formError ? (
+              <div className="error_label">Something's wrong. Try Again!</div>
+            ) : null}
+            <div className="success_label">{this.state.formSuccess}</div>
+            <Button
+              variant="contained"
+              size="large"
+              onClick={(event) => this.submitForm(event)}
+            >
+              {this.props.btnText ? this.props.btnText : "Send Message"}
+            </Button>
+          </form>
+        </div>
+      </Roll>
+    );
+  }
 }
 
 export default MessageUs;
